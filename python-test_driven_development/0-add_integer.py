e9d6b07d8655538eeb0a27c0f0f290586e251100
Atilla <muradliatilla@gmail.com>
2026-02-22 18:56:34 +0000

    intejer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Module that contains add_integer function."""
-
-
 def add_integer(a, b=98):
     """Add 2 integers.
-
     a and b must be integers or floats, otherwise raise TypeError.
     Floats are casted to integers before addition.
 
@@ -15,15 +12,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
-    try:
-        a = int(a)
-    except (ValueError, OverflowError):
-        raise TypeError("a must be an integer")
-
-    try:
-        b = int(b)
-    except (ValueError, OverflowError):
-        raise TypeError("b must be an integer")
-
-    return a + b)
+    return int(a) + int(b)
