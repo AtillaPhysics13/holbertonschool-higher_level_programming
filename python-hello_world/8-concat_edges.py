057cea4a0c1dc93e3afde6223fb2c5638905f128
Atilla <muradliatilla@gmail.com>
2026-02-22 13:41:56 +0000

    object

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str1="with python"
+str2=str[32:58]
+print(f"{str2} {str1)
