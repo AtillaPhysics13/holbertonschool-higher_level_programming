3f377e62629827a27d6405e3433f85a387b19259
Atilla <muradliatilla@gmail.com>
2026-02-22 13:46:18 +0000

    ob

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str1="with python"
-str2=str[32:58]
-print(f"{str2} {str1)
+str2 = "object-oriented programming"
+str1 = "with python"
+str = str2 +" "+str1
+print(str)
