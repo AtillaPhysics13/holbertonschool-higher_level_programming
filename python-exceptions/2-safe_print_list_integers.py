8e90222343207ce5441230cb31d47a5f3bc2fc7a
Atilla <muradliatilla@gmail.com>
2026-02-22 17:41:23 +0000

    duzenlendi

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,8 +6,10 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except TypeError:
-            continue
+            pass
         except IndexError:
+            pass
+        finally:
             continue
     print()
     return count
